ea3ffeeb9fd348fa2fb509518083ff2fbbdc8d1a
eduardo91811-tech <eduardo.91811@gmail.com>
2026-06-30 17:11:31 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+// Sube este número cada vez que subas cambios a index.html
+// para forzar que el navegador descargue la versión nueva.
+const CACHE_VERSION = 'mientreno-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_VERSION).map((k) => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
